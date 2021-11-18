9c122f975fc4933f82613b2e9e5bf0864e93ab98
Boris Diakur <borisdiakur@gmail.com>
2021-11-18 08:37:53 +0100

    fix build

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,43 +3,12 @@
 	import '@emdgroup-liquid/liquid/dist/css/liquid.global.css'
 
 	import { browser } from '$app/env'
-	import { setAssetPath } from '@emdgroup-liquid/liquid'
-	import {
-		LdBgCells,
-		LdButton,
-		LdCheckbox,
-		LdIcon,
-		LdInput,
-		LdInputMessage,
-		LdLabel,
-		LdNotification,
-		LdOption,
-		LdOptionInternal,
-		LdSelectPopper,
-		LdSelect,
-		LdTooltip,
-		LdTooltipPopper,
-		LdTypo
-	} from '@emdgroup-liquid/liquid/dist/custom-elements'
+	import { defineCustomElements, setAssetPath } from '@emdgroup-liquid/liquid'
 
 	let ready = false
 	if (browser) {
 		setAssetPath(window.location.origin)
-		customElements.define('ld-bg-cells', LdBgCells)
-		customElements.define('ld-button', LdButton)
-		customElements.define('ld-checkbox', LdCheckbox)
-		customElements.define('ld-typo', LdTypo)
-		customElements.define('ld-icon', LdIcon)
-		customElements.define('ld-input', LdInput)
-		customElements.define('ld-input-message', LdInputMessage)
-		customElements.define('ld-label', LdLabel)
-		customElements.define('ld-notification', LdNotification)
-		customElements.define('ld-option', LdOption)
-		customElements.define('ld-option-internal', LdOptionInternal)
-		customElements.define('ld-select-popper', LdSelectPopper)
-		customElements.define('ld-select', LdSelect)
-		customElements.define('ld-tooltip', LdTooltip)
-		customElements.define('ld-tooltip-popper', LdTooltipPopper)
+		defineCustomElements()
 		ready = true
 	}
 </script>
